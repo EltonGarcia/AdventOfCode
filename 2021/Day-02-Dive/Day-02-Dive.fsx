1e567164a2f6d5e77da0e6eaca921dde5726d568
Elton Santana <elton.garcia.s@gmail.com>
2021-12-25 16:16:20 -0300

    Improve Day 2 - 2021, use fold

--- 2021/Day-02-Dive/Day-02-Dive.fsx
+++ 2021/Day-02-Dive/Day-02-Dive.fsx
@@ -6,36 +6,23 @@ let parseEntry (input: string) : string * int =
 let commands = File.ReadAllLines "input.txt" |> Array.map parseEntry |> Array.toList
 
 
-let applyCommand (command, distance, depth) =
+let applyCommand (distance, depth) command =
     match command with
     | "up", v -> distance, depth - v
     | "down", v -> distance, depth + v
     | "forward", v -> distance + v, depth
     | _, _ -> 0,0
 
-
-let rec calculate (list: List<string * int>, distance: int, depth: int) =
-    match list with
-    | x::xs -> applyCommand (x, distance, depth) |> (fun (h,d) -> calculate(xs, h, d)) 
-    | [] -> (distance, depth)
+let part1 = List.fold applyCommand (0, 0) commands ||> (*)
 
 
-let horizotal, depth = calculate (commands, 0, 0)
-let part1 = horizotal * depth
-
-let applyCommand2 (command, aim, distance, depth) =
+let applyCommand2 (distance, depth, aim) command =
     match command with
-    | "up", v -> aim - v, distance, depth
-    | "down", v -> aim + v, distance, depth
-    | "forward", v -> aim, distance + v, depth + (aim * v)
+    | "up", v -> distance, depth, aim - v
+    | "down", v -> distance, depth, aim + v
+    | "forward", v -> distance + v, depth + (aim * v), aim
     | _, _ -> 0,0,0
 
-
-let rec calculate2 (list: List<string * int>, aim: int, distance: int, depth: int) =
-    match list with
-    | x::xs -> applyCommand2 (x, aim, distance, depth) |> (fun (a,h,d) -> calculate2(xs, a, h, d)) 
-    | [] -> (aim, distance, depth)
-
-
-let aim, horizotal2, depth2 = calculate2 (commands, 0, 0, 0)
-let part2 = horizotal2 * depth2
+let part2 = List.fold applyCommand2 (0, 0, 0) commands 
+            |> fun (h, d, _) -> (h,d) 
+            ||> (*)
