6be55706d109f2434bccc3bd9e8242c741621269
Elton Santana <elton.garcia.s@gmail.com>
2024-12-29 23:54:21 -0300

    2024 - day 01 - python solution

---
+++ 2024/01/solution.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from fileinput import input
+
+a = []
+b = []
+for line in input():
+    arr = line.split('   ')
+    a.append(int(arr[0]))
+    b.append(int(arr[1]))
+
+a.sort()
+b.sort()
+
+sum = 0
+for i in range(0, len(a)):
+    v = b[i] - a[i]
+    sum += v if v > 0 else v*-1
+
+print(sum)
+# Part 1 - result: 2580760
+
+ridx = 0
+sum2 = 0
+for left in a:
+    for i in range(ridx, len(a)):
+        right = b[i]
+        if left == right:
+            sum2 += left
+        elif left < right:
+            ridx = i
+            break
+
+print(sum2)
+# Part 2 - result: 25358365
