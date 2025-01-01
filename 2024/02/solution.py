112f91ee58b7e1830861de58330aca73edc55542
Elton Santana <elton.garcia.s@gmail.com>
2025-01-01 12:57:40 -0300

    2024 - day 02 - python solution - part 2

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -3,9 +3,9 @@
 from fileinput import input
 
 reports = [[int(x) for x in line.split()] for line in input()]
-#rps = [list(zip(x, x[1:])) for x in m]
 
-def getInvalidIndex(l) -> int:
+def getInvalidIndex(rp) -> int:
+    l = list(zip(rp, rp[1:]))
     increasing = True
     invalidIdx = -1
     for i in range(0, len(l)):
@@ -24,15 +24,40 @@ def getInvalidIndex(l) -> int:
 
     return invalidIdx
 
-r1 = 0
-for rp in reports:
-    l = list(zip(rp, rp[1:]))
+def listExcludingIdx(l, idx):
+    return l[:idx] + l[idx+1 :]
 
-    invalidIdx = getInvalidIndex(l)
+r1 = 0
+r2 = 0
+for rp in reports:
+    invalidIdx = getInvalidIndex(rp)
     if invalidIdx == -1:
         r1 += 1
-    
+        r2 += 1
+    else:
+        rpD1 = listExcludingIdx(rp, invalidIdx)
+        noCur = getInvalidIndex(rpD1)
+        if noCur == -1:
+            r2 += 1
+            continue
+
+        if invalidIdx-1 >= 0:
+            rpD2 = listExcludingIdx(rp, invalidIdx-1)
+            noPrev = getInvalidIndex(rpD2)
+            if noPrev == -1:
+                r2 += 1
+                continue
+
+        if invalidIdx+1 < len(rp):
+            rpD3 = listExcludingIdx(rp, invalidIdx+1)
+            noNext = getInvalidIndex(rpD3)
+            if noNext == -1:
+                r2 += 1
+                continue
+
 print("Part1:", r1)
 print("Expected:", 432)
 
-
+print("")
+print("Part2:", r2)
+print("Expected:", 488)
